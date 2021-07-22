372066f82bfa6a286e54ac2a4661be0c45349487
Vitalii Havryliuk <vitalii.havryliuk@pragma-technologies.com>
2021-07-22 12:15:06 +0300

    Add AbiCoder

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
+const { ethers } = require('ethers')
 const { Requester, Validator } = require('@chainlink/external-adapter')
+
+const AbiCoder = ethers.utils.AbiCoder
+const ADDRESS_PREFIX_REGEX = /^(41)/
+const ADDRESS_PREFIX = '41'
+
+function encodeParams (inputs) {
+  let parameters = ''
+
+  if (inputs.length === 0) return parameters
+  const abiCoder = new AbiCoder()
+  const types = []
+  const values = []
+
+  for (let i = 0; i < inputs.length; i++) {
+    let { type, value } = inputs[i]
+    if (type === 'address') value = value.replace(ADDRESS_PREFIX_REGEX, '0x')
+    else if (type === 'address[]') value = value.map(v => v.toString('hex').replace(ADDRESS_PREFIX_REGEX, '0x'))
+    types.push(type)
+    values.push(value)
+  }
+
+  try {
+    parameters = abiCoder.encode(types, values).replace(/^(0x)/, '')
+  } catch (ex) {
+    console.log(ex)
+  }
+  return parameters
+}
+
+function decodeParams (types, output, ignoreMethodHash) {
+  if (!output || typeof output === 'boolean') {
+    ignoreMethodHash = output
+    output = types
+  }
+
+  if (ignoreMethodHash && output.replace(/^0x/, '').length % 64 === 8) {
+    output = '0x' + output.replace(/^0x/, '').substring(8)
+  }
+
+  const abiCoder = new AbiCoder()
+
+  if (output.replace(/^0x/, '').length % 64) {
+    throw new Error('The encoded string is not valid. Its length must be a multiple of 64.')
+  }
+  return abiCoder.decode(types, output).reduce((obj, arg, index) => {
+    if (types[index] === 'address') arg = ADDRESS_PREFIX + arg.substr(2).toLowerCase()
+    obj.push(arg)
+    return obj
+  }, [])
+}
 
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
@@ -13,11 +64,13 @@ const createRequest = (input, callback) => {
   const jobRunID = validator.validated.id
   const url = 'https://api.shasta.trongrid.io/wallet/triggerconstantcontract'
 
+  const parameter = encodeParams([{ type: 'address', value: JSON.parse(input.data.parameter).value }])
+
   const data = {
     owner_address: 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb',
     contract_address: 'TBSo1pthwZJkkXLwfNUC3wzKG2K7wt2Zvg',
     function_selector: 'confirmedTokensForUser(address)',
-    parameter: JSON.parse(input.data.parameter).value,
+    parameter,
     visible: true
   }
 
@@ -39,7 +92,7 @@ const createRequest = (input, callback) => {
       // It's common practice to store the desired value at the top-level
       // result key. This allows different adapters to be compatible with
       // one another.
-      response.data.result = '0x' + Requester.getResult(response.data, ['constant_result', '0'])
+      response.data.result = decodeParams(['uint256', 'uint256'], '0x' + Requester.getResult(response.data, ['constant_result', '0']), false)
       callback(response.status, Requester.success(jobRunID, response))
     })
     .catch(error => {
